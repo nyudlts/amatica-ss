2b67c06d0b45e15bb24413bf5853f4abdb2a9df1
Donald <don.mennerich@nyu.edu>
2019-11-22 14:54:54 -0500

    initial commit

---
+++ Vagrantfile
@@ -0,0 +1,5 @@
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "centos/7"
+  config.vm.network "forwarded_port", guest: 8001, host: 8000
+end
---
+++ install.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+# install core packages, nginx and mysql
+sudo yum install -y epel-release policycoreutils-python nginx mariadb
+
+# open ports and allow server access to network resources
+sudo semanage port -m -t http_port_t -p tcp 81
+sudo semanage port -a -t http_port_t -p tcp 8001
+sudo setsebool -P httpd_can_network_connect_db=1
+sudo setsebool -P httpd_can_network_connect=1
+sudo setsebool -P httpd_setrlimit 1
+
+# add the archivematica repositories
+sudo -u root bash -c 'cat << EOF > /etc/yum.repos.d/archivematica.repo
+[archivematica]
+name=archivematica
+baseurl=https://packages.archivematica.org/1.10.x/centos
+gpgcheck=1
+gpgkey=https://packages.archivematica.org/1.10.x/key.asc
+enabled=1
+EOF'
+
+sudo -u root bash -c 'cat << EOF > /etc/yum.repos.d/archivematica-extras.repo
+[archivematica-extras]
+name=archivematica-extras
+baseurl=https://packages.archivematica.org/1.10.x/centos-extras
+gpgcheck=1
+gpgkey=https://packages.archivematica.org/1.10.x/key.asc
+enabled=1
+EOF'
+
+# install the archivematica storage service
+sudo yum install -y python-pip archivematica-storage-service
