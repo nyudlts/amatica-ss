76b1277461f3548a5fd9a1947e5eda3438303a62
Donald <don.mennerich@nyu.edu>
2020-01-06 14:52:14 -0500

    updating nginx conf

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,8 @@
 
 Vagrant.configure("2") do |config|
   config.vm.box = "centos/7"
-  config.vm.network "forwarded_port", guest: 8001, host: 8000
+  config.vm.network "private_network", ip: "192.168.66.66"
+  config.vm.provision "file", source: "nginx.conf", destination: "/tmp/nginx.conf"
+  config.vm.provision "file", source: "install.sh", destination: "install.sh"
+  config.vm.provision "shell", path: "install.sh"
 end
--- install.sh
+++ install.sh
@@ -1,16 +1,27 @@
 #! /bin/sh
 
 # install core packages, nginx and mysql
-sudo yum install -y epel-release policycoreutils-python python-pip nginx mariadb
+sudo yum install -y epel-release policycoreutils-python python-pip yum-utils mariadb
 
 # open ports and allow server access to network resources
-sudo semanage port -a -t http_port_t -p tcp 8000
-sudo setsebool -P httpd_can_network_connect_db=1
-sudo setsebool -P httpd_can_network_connect=1
-sudo setsebool -P httpd_setrlimit 1
+#semanage port -a -t http_port_t -p tcp 8000
+#setsebool -P httpd_can_network_connect_db=1
+#setsebool -P httpd_can_network_connect=1
+#setsebool -P httpd_setrlimit 1
+
+#intsall nginx repository
+bash -c 'cat << EOF > /etc/yum.repos.d/nginx.repo
+[nginx-stable]
+name=nginx stable repo
+baseurl=http://nginx.org/packages/centos/7/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
+EOF'
 
 # add the archivematica repositories
-sudo -u root bash -c 'cat << EOF > /etc/yum.repos.d/archivematica.repo
+bash -c 'cat << EOF > /etc/yum.repos.d/archivematica.repo
 [archivematica]
 name=archivematica
 baseurl=https://packages.archivematica.org/1.10.x/centos
@@ -19,7 +30,7 @@ gpgkey=https://packages.archivematica.org/1.10.x/key.asc
 enabled=1
 EOF'
 
-sudo -u root bash -c 'cat << EOF > /etc/yum.repos.d/archivematica-extras.repo
+bash -c 'cat << EOF > /etc/yum.repos.d/archivematica-extras.repo
 [archivematica-extras]
 name=archivematica-extras
 baseurl=https://packages.archivematica.org/1.10.x/centos-extras
@@ -29,8 +40,26 @@ enabled=1
 EOF'
 
 # install the archivematica storage service
-sudo yum install -y archivematica-storage-service
+yum install -y archivematica-storage-service
 
-sudo -u root systemctl enable archivematica-storage-service
-sudo -u root systemctl enable nginx
-sudo -u root systemctl enable rngd
+#enable the services
+systemctl enable archivematica-storage-service
+systemctl start archivematica-storage-service
+systemctl enable rngd
+systemctl start rngd
+
+# init the sqlite base
+sudo -u archivematica bash -c " \
+set -a -e -x
+source /etc/sysconfig/archivematica-storage-service
+cd /usr/lib/archivematica/storage-service
+/usr/share/archivematica/virtualenvs/archivematica-storage-service/bin/python manage.py migrate";
+
+#config and start nginx
+cp /tmp/nginx.conf /etc/nginx/nginx.conf
+
+sed -i -e 's/listen 8001 default_server/listen 80 default_server/' /etc/nginx/conf.d/archivematica-storage-service.conf
+
+systemctl enable nginx
+
+systemctl start nginx
